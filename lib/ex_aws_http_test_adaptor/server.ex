c20a9eb5eb8ed90b0a830de45bebedf466590b1a
Jon Rowe <hello@jonrowe.co.uk>
2020-09-11 11:56:21 +0100

    Basic server for setting expected respponses

--- lib/ex_aws_http_test_adaptor/server.ex
+++ lib/ex_aws_http_test_adaptor/server.ex
@@ -16,4 +16,30 @@ defmodule ExAwsHttpTestAdaptor.Server do
   end
 
   def start_link(args), do: GenServer.start_link(__MODULE__, args, name: __MODULE__)
+
+  def handle_call({:set, pid, method, path, status, headers, body}, _, %{calls: calls}) do
+    {:reply,:ok, %{calls: Map.put(calls, pid, set_call(get(calls, pid), method, path, status, headers, body))}}
+  end
+
+  def handle_call({:request, pid, method, path, _body, _headers, _opts}, _, state = %{calls: calls}) do
+    {:reply, request(get(calls, pid), method, path), state}
+  end
+
+  def handle_call(_, _, state), do: {:noreply, state}
+
+  defp get(map, key), do: Map.get(map, key, %{})
+
+  defguardp map_get(map, key) when :erlang.map_get(key, map)
+
+  defp request(call, method, path) when is_map_key(call, path) and is_map_key(map_get(call, path), method)do
+    call
+    |> Map.get(path)
+    |> Map.get(method)
+  end
+
+  defp request(_, _, _), do: {404, [], []}
+
+  defp set_call(map, method, path, status, headers, body) do
+    Map.put(map, path, Map.put(get(map, path), method, {status, headers, body}))
+  end
 end
--- test/ex_aws_http_test_adaptor/server_test.exs
+++ test/ex_aws_http_test_adaptor/server_test.exs
@@ -12,4 +12,24 @@ defmodule ExAwsHttpTestAdaptor.ServerTest do
       assert is_pid(Process.whereis(Server)) == true
     end
   end
+
+  #def request(method, url, req_body, headers, http_opts) do
+  describe "handle_call/2" do
+    setup :find_server
+
+    test "it defaults to a 404", %{pid: pid} do
+      assert {404, [], []} == GenServer.call(pid, {:request, self(), :get, "/some/path", "", [], []})
+    end
+
+    test "it allows setting a response", %{pid: pid} do
+      assert :ok == GenServer.call(pid, {:set, self(), :get, "/some/path", 200, [], ["OK"]})
+    end
+
+    test "it returns a previously set response", %{pid: pid} do
+      GenServer.call(pid, {:set, self(), :get, "/some/path", 200, [], ["OK"]})
+      assert {200, [], ["OK"]} == GenServer.call(pid, {:request, self(), :get, "/some/path", "", [], []})
+    end
+  end
+
+  defp find_server(_), do: {:ok, pid: Process.whereis(Server)}
 end
