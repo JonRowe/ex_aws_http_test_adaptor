2ebf46b5a1d19ff13e101b464b1cbbfeeed1a90f
Jon Rowe <hello@jonrowe.co.uk>
2020-09-11 12:12:29 +0100

    Add HTTP adaptor implementation

--- lib/ex_aws_http_test_adaptor.ex
+++ lib/ex_aws_http_test_adaptor.ex
@@ -3,4 +3,32 @@ defmodule ExAwsHttpTestAdaptor do
   A test http adaptor for ExAws, allows testing responses from AWS by
   swapping out the HTTP adaptor.
   """
+
+  alias ExAwsHttpTestAdaptor.Server
+
+  @behaviour ExAws.Request.HttpClient
+
+  require Logger
+
+  @doc """
+  Make a fake request to url with supplied body, headers and opts.
+  """
+  @impl true
+  def request(method, url, req_body, headers, http_opts) do
+    Logger.debug "Call made to #{url}"
+    {status, _headers, body} = GenServer.call(Server, {:request, self(), method, url, req_body, headers, http_opts})
+    {:ok, %{status_code: status, body: body}}
+  end
+
+  @doc """
+  Set a fake response to url with supplied body, headers and opts.
+  """
+  def set(url, body \\ [], opts \\ []) do
+    headers = Keyword.get(opts, :headers, [])
+    method = Keyword.get(opts, :method, :get)
+    pid = Keyword.get(opts, :pid, self())
+    status = Keyword.get(opts, :status, 200)
+
+    GenServer.call(Server, {:set, pid, method, url, status, headers, body})
+  end
 end
--- test/ex_aws_http_test_adaptor_test.exs
+++ test/ex_aws_http_test_adaptor_test.exs
@@ -1,4 +1,9 @@
 defmodule ExAwsHttpTestAdaptorTest do
   use ExUnit.Case
   doctest ExAwsHttpTestAdaptor
+
+  test "it will default to the current processes requests" do
+    ExAwsHttpTestAdaptor.set("https://amazon.com/s3/", "OK")
+    assert {:ok, %{status_code: 200, body: "OK"}} = ExAwsHttpTestAdaptor.request(:get, "https://amazon.com/s3/", "", [], [])
+  end
 end
