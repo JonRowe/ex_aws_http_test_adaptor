6b15b90876279508f50b1a925d1878357948bc29
Jon Rowe <hello@jonrowe.co.uk>
2020-09-25 10:00:25 +0100

    Demonstrate cross process communication

--- test/ex_aws_http_test_adaptor_test.exs
+++ test/ex_aws_http_test_adaptor_test.exs
@@ -9,6 +9,36 @@ defmodule ExAwsHttpTestAdaptorTest do
              ExAwsHttpTestAdaptor.request(:get, "https://amazon.com/s3/", "", [], [])
   end
 
+  test "it isolates processes" do
+    Task.start(fn -> ExAwsHttpTestAdaptor.set("https://amazon.com/s3/", "OK") end)
+
+    assert {:ok, %{status_code: 404, body: []}} =
+             ExAwsHttpTestAdaptor.request(:get, "https://amazon.com/s3/", "", [], [])
+  end
+
+  test "it supports across process communication" do
+    pid = self()
+
+    Task.start(fn ->
+      ExAwsHttpTestAdaptor.set("https://amazon.com/s3/", "OK", pid: pid)
+      send(pid, :setup)
+    end)
+
+    # this blocks until the task has run
+    receive do
+      :setup -> :done
+    end
+
+    assert {:ok, %{status_code: 200, body: "OK"}} =
+             ExAwsHttpTestAdaptor.request(:get, "https://amazon.com/s3/", "", [], [])
+  end
+
+  test "it will allow refuting a url combo" do
+    ExAwsHttpTestAdaptor.refute("https://amazon.com/s3/", method: :delete)
+
+    assert_raise RuntimeError, fn -> ExAwsHttpTestAdaptor.request(:delete, "https://amazon.com/s3/", "", [], []) end
+  end
+
   describe "calls/0" do
     test "it will return current pid calls by default" do
       assert ExAwsHttpTestAdaptor.calls() == []
@@ -17,11 +47,5 @@ defmodule ExAwsHttpTestAdaptorTest do
 
       assert ExAwsHttpTestAdaptor.calls() == [{:get, "https://amazon.com/s3/", [], ""}]
     end
-  end
-
-  test "it will allow refuting a url combo" do
-    ExAwsHttpTestAdaptor.refute("https://amazon.com/s3/", method: :delete)
-
-    assert_raise RuntimeError, fn -> ExAwsHttpTestAdaptor.request(:delete, "https://amazon.com/s3/", "", [], []) end
   end
 end
